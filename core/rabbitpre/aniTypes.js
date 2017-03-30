977043f044fa6e8daf1e311bc82ec79ecbcba730
zcj <zhengchangjun@eqxiu.com>
2017-03-30 19:01:07 +0800

    bug

--- core/rabbitpre/aniTypes.js
+++ core/rabbitpre/aniTypes.js
@@ -124,139 +124,46 @@ module.exports = {
 		direction: 0,
 		name: '闪烁'
 	},
-	'bounceIn': {
-		type: 27,
-		direction: 0
-	},
-	'rubberBand': {
-		type: 5,
-		direction: 0
-	},
 	'wobble': {
 		type: 6,
-		direction: 0
+		direction: 0,
+		name: '摇摆'
 	},
-	'rotateIn': {
-		type: 7,
-		direction: 0
-	},
-	'flip': {
-		type: 8,
-		direction: 0
-	},
-	'swing': {
-		type: 9,
-		direction: 0
+	'tada': {
+		type: 6,
+		direction: 0,
+		name: '摇摆'
 	},
 	'fadeOut': {
 		type: 10,
-		direction: 0
-	},
-	'flipOutY': {
-		type: 11,
-		direction: 0
-	},
-	'rollIn': {
-		type: 12,
-		direction: 0
-	},
-	'lightSpeedIn': {
-		type: 13,
-		direction: 0
-	},
-	'rollOut': {
-		type: 15,
-		direction: 0
-	},
-	'lightSpeedOut': {
-		type: 16,
-		direction: 0
-	},
-	'fadeOutRight': {
-		type: 17,
-		direction: 0
-	},
-	'fadeOutDown': {
-		type: 17,
-		direction: 1
+		direction: 0,
+		name: '淡出'
 	},
 	'fadeOutLeft': {
 		type: 17,
-		direction: 2
+		direction: 0,
+		name: '从左向右移出'
 	},
 	'fadeOutUp': {
 		type: 17,
-		direction: 3
+		direction: 1,
+		name: '从上到下移出'
 	},
-	'bounceOutRight': {
-		type: 19,
-		direction: 0
+	'fadeOutRight': {
+		type: 17,
+		direction: 2,
+		name: '从右向左移出'
 	},
-	'bounceOutDown': {
-		type: 19,
-		direction: 1
-	},
-	'bounceOutLeft': {
-		type: 19,
-		direction: 2
-	},
-	'bounceOutUp': {
-		type: 19,
-		direction: 3
-	},
-	'flipInY': {
-		type: 20,
-		direction: 0
-	},
-	'tada': {
-		type: 21,
-		direction: 0
-	},
-	'jello': {
-		type: 22,
-		direction: 0
-	},
-	'flipInX': {
-		type: 24,
-		direction: 0
-	},
-	'bounceOutLeft': {
-		type: 24,
-		direction: 0
-	},
-	'flipOutX': {
-		type: 25,
-		direction: 0
-	},
-	'twisterInDown': {
-		type: 26,
-		direction: 0
-	},
-	'puffIn': {
-		type: 27,
-		direction: 0
-	},
-	'puffOut': {
-		type: 28,
-		direction: 0
-	},
-	'twisterInUp': {
-		type: 31,
-		direction: 0
-	},
-	'vanishOut': {
-		type: 32,
-		direction: 0
-	},
-	'vanishIn': {
-		type: 33,
-		direction: 0
+	'fadeOutDown': {
+		type: 17,
+		direction: 3,
+		name: '从下到上移出'
 	},
 	'translate': {
 		type: 7,
 		direction: 0,
 		name: '旋转'
-	}
+	},
 	'translateReverse': {
 		type: 7,
 		direction: 0,

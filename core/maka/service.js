66da4697bd6db01cd0dee55c69b5f302989457e7
zcj <zhengchangjun@eqxiu.com>
2017-04-01 17:50:39 +0800

    bug

--- core/maka/service.js
+++ core/maka/service.js
@@ -8,6 +8,16 @@ var request = require('./../request');
 var config = require('./../config').maka;
 var serverHost = config.severHost;
 
+var _headers;
+
+/**
+ * [setHeaders 设置http header]
+ * @param {[type]} headers [description]
+ */
+function setHeaders(headers) {
+	_headers = headers;
+}
+
 function createScene() {
 	return request.post({
 		data: querystring.stringify(data),
--- test/index.js
+++ test/index.js
@@ -9,12 +9,15 @@
 var config = require('./../core/config').maka;
 var MakaUser = require('./../core/user/makaUser');
 var user = new MakaUser(config.userName, config.userPwd);
+var makaService = require('./../maka/service')
 user.login().then(res=>{
-	for(var i = 0;i<user.cookie.length;i++) {
-		var cookie = user.cookie[i];
-		var reg = /Makauid=([\d]+);/
-		if(reg.test(cookie)) {
-			console.log(cookie.match(reg)[1]);
-		}
-	}
+	// for(var i = 0;i<user.cookie.length;i++) {
+	// 	var cookie = user.cookie[i];
+	// 	var reg = /Makauid=([\d]+);/
+	// 	if(reg.test(cookie)) {
+	// 		console.log(cookie.match(reg)[1]);
+	// 	}
+	// }
+	makaService.setHeaders({Origin: config.origin, cookie: user.cookie});
+	makaService.createScene().then(res=>console.log(res));
 });

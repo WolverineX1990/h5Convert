4f700e21ed5a64488c828df187267351799ff395
zcj <zhengchangjun@eqxiu.com>
2017-04-11 18:17:10 +0800

    'bug'

--- test/makaSign.js
+++ test/makaSign.js
@@ -16,11 +16,14 @@ function test() {
 	// var path = '';
 	// var signature = sign(token, header, path);
 	// console.log(signature);
-	
+	var arr = ["PUT","22AgwBG4ncftPnqbbxTb5Q==","image/jpeg","Tue, 11 Apr 2017 10:15:01 GMT","x-oss-date:Tue, 11 Apr 2017 10:15:01 GMT","x-oss-security-token:CAISwwJ1q6Ft5B2yfSjIraraO/3Cm4l3hrCnZ1HZ0FpkVM1mro+dqDz2IHBLdHltBewdsvU/lGtV6Pcdlq4oGsYYHhSZNV+5QBmKslHPWZHInuDox05t6vT8a5T6aXPS2MvVfJ+nLrf0ceusbFbpjzJ6xaCAGxypQ12iN+//6/tgdc9FcQSkSjBECdxKRG5ls9RIDWbNEvyvPxX2+FGyanBloQ1hk2hyxL2iy8mHkHrkgUb91/UeqvauQMWtZNI+O4xkAZXnhbcmLfOZjHEBsEMSpPws0/5B8DTOptCWH0RL7lKbKOfOtYY+dVIiPvV9WYw89aOszqQg5rOCzN+vm00XYts4CXqPGNqSp+LfA/6sO9oDcrL2Bm/AyNjnNOOu41x0MS9HaFgWKopxcSIuWEM2LjbBMeq85EuPbBqnT7hKYVixa0+npRqAAQ3boHvlboIS3M2EyVoi+UmpnFwRighlszaRh4fz+7wJbEZydav11AjJGBB0z155mQF0JaTOZyjX26Vh+Xbst7MqpF08WuL3JSKtr5Ia1sBjnVChViVhmXG3ptRGl3lc4fOBuxc4cPg+KQTWteA+8cvEqn7i1AG401nNQw83Iaiy","/makapicture/user/3915305/images/205462a7ae2c6f78286fc85571828f2f.jpg"];
+	var secret = 'BBpiPuScYpqwrUdZN8sGEgpDU4Cz7kj8XEPddq7VNKMR';
+	var string = arr.join('\n');
 
-	var hmac = crypto.createHmac('sha1', app_secret);
-	hmac.update('foo');
-	hmac.digest('hex');
+	var hmac = crypto.createHmac('sha1', secret);
+	hmac.update(string);
+	var tt = hmac.digest('base64');
+	console.log(tt);
 }
 //crypto.hmac
 

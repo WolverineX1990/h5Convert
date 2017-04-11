cca5c50e442903fa782f199a233a964723e0eff0
zcj <1248198090@qq.com>
2017-04-11 22:51:40 +0800

    bug

--- core/maka/sign.js
+++ core/maka/sign.js
@@ -1,15 +1,14 @@
 var utils = require('./../utils');
+var crypto = require('crypto');
 
-function sign(token, headers) {
-	var signature = sha1(token.SecretAccessKey, stringToSign(headers, path));
+function sign(token, headers, path) {
+	var hmac = crypto.createHmac('sha1', token.SecretAccessKey);
+	hmac.update(stringToSign(headers, path));
+	var signature = hmac.digest('base64');
 	var auth = 'OSS ' + token.AccessKeyId + ':' + signature;
 	return auth;
 }
 
-function sha1(key, sign) {
-
-	return '';
-}
 
 function stringToSign(headers, path) {
 	var parts = [];
@@ -17,8 +16,24 @@ function stringToSign(headers, path) {
 	parts.push(headers['Content-MD5'] || '');
 	parts.push(headers['Content-Type'] || '');
 	parts.push(headers['presigned-expires'] || headers['x-oss-date'] || headers['Date'] || '');
-	parts.push(path);
+	parts.push(canonicalizedHeaders(headers));
 	return parts.join('\n');
+}
+
+function canonicalizedHeaders(headers) {
+    var arr = [];
+    utils.each(headers, function(name) {
+        if (name.match(/^x-oss-/i))
+            arr.push(name);
+    });
+    arr.sort(function(a, b) {
+        return a.toLowerCase() < b.toLowerCase() ? -1 : 1;
+    });
+    var parts = [];
+    utils.each(headers, function(name) {
+        parts.push(name.toLowerCase() + ':' + String(headers[name]));
+    });
+    return parts.join('\n');
 }
 
 module.exports = sign;
--- test/makaSign.js
+++ test/makaSign.js
@@ -1,30 +1,22 @@
 'use strict';
 var sign = require('./../core/maka/sign');
-var crypto = require('crypto');
 function test() {
 	var token = {
-		"accessKeyId": "STS.DqUMpZSZpFTkCSHEX8DWzzaao",
-		"secretAccessKey": "Dreq4i6nQnc1M8NjNcdhNfbC38TxbQQXjPGfgLVCahFJ",
-		"securityToken": "CAISwwJ1q6Ft5B2yfSjIpbLgBsruvoVR8ZaAQXX5oVhtSNhWlaTKjTz2IHBLdHltBewdsvU/lGtV6Pcdlq4oGsYYHhSZNWOOJB2KslHPWZHInuDox05t6vT8a5T6aXPS2MvVfJ+nLrf0ceusbFbpjzJ6xaCAGxypQ12iN+//6/tgdc9FcQSkSjBECdxKRG5ls9RIDWbNEvyvPxX2+FGyanBloQ1hk2hyxL2iy8mHkHrkgUb91/UeqvauQMWtZNI+O4xkAZXnhbcmLfOZjHEBsEMSpPws0/5B8DTOptCWH0RL7lKbKOfOtYY+dVIiPvV9WYw89aOszqQg5rOCzN+vm00XYts4CXqPGNqSp+LfA/6sO9oDcrL2Bm/AyNjnNOOu41x0MS9HaFgWKopxcSIuWEM2LjbBMeq85EuPbBqnT7hKYVixa0+npRqAAR7XyLOGdnHR2EXNF50oYgvB5p/XNxtrU4AvtEI0XSm4R4ZpMJHiaZFw5POrgH7GqFKDk+XMjFexF6Z9ys5dIVtFOw8crPKncAjxWMLaK3gymL9B+XmMv3PPjgVzs67ibmhe0givE4H0wqjiUQ2d8YfoVvI4rw2gU/wpVTAjSG8r"
+		"accessKeyId": "STS.LXh2fet7VqXDLTrPN7qNxLr74",
+		"secretAccessKey": "CwQEKyLv9JfA6ZLTfakdT1gSFisn5jUDckCZai2UsxMN",
+		"securityToken": "CAISwwJ1q6Ft5B2yfSjIrZvdedzRmeh3xpqvTnLDtE5ifcFUo7ec1jz2IHBLdHltBewdsvU/lGtV6Pcdlq4oGsYYHhSZNWfCUxaKslHPWZHInuDox05t6vT8a5T6aXPS2MvVfJ+nLrf0ceusbFbpjzJ6xaCAGxypQ12iN+//6/tgdc9FcQSkSjBECdxKRG5ls9RIDWbNEvyvPxX2+FGyanBloQ1hk2hyxL2iy8mHkHrkgUb91/UeqvauQMWtZNI+O4xkAZXnhbcmLfOZjHEBsEMSpPws0/5B8DTOptCWH0RL7lKbKOfOtYY+dVIiPvV9WYw89aOszqQg5rOCzN+vm00XYts4CXqPGNqSp+LfA/6sO9oDcrL2Bm/AyNjnNOOu41x0MS9HaFgWKopxcSIuWEM2LjbBMeq85EuPbBqnT7hKYVixa0+npRqAARMnwj8x1oN67WhjoYr8KptO80MkIH6BJa4KIXN5iIkRhqVVfLtAOxHHe9oq/zlhRFXsRyhlIbC2rx/O9uEPVRGGOycvCZLExnsh9UhLhpe+yiSJPU425d9EUbiOq5MVQWtv+Z4l3P4pFmz7AEYAHdrTUi5wvkwJoFqjmMf8iDXi"
 	};
-	// var header = {
-	// 	'method': 'PUT',
-	// 	'Content-MD5': '',
-	// 	'Content-Type': '',
-	// 	'x-oss-date': ''
-	// };
-	// var path = '';
-	// var signature = sign(token, header, path);
-	// console.log(signature);
-	var arr = ["PUT","22AgwBG4ncftPnqbbxTb5Q==","image/jpeg","Tue, 11 Apr 2017 10:15:01 GMT","x-oss-date:Tue, 11 Apr 2017 10:15:01 GMT","x-oss-security-token:CAISwwJ1q6Ft5B2yfSjIraraO/3Cm4l3hrCnZ1HZ0FpkVM1mro+dqDz2IHBLdHltBewdsvU/lGtV6Pcdlq4oGsYYHhSZNV+5QBmKslHPWZHInuDox05t6vT8a5T6aXPS2MvVfJ+nLrf0ceusbFbpjzJ6xaCAGxypQ12iN+//6/tgdc9FcQSkSjBECdxKRG5ls9RIDWbNEvyvPxX2+FGyanBloQ1hk2hyxL2iy8mHkHrkgUb91/UeqvauQMWtZNI+O4xkAZXnhbcmLfOZjHEBsEMSpPws0/5B8DTOptCWH0RL7lKbKOfOtYY+dVIiPvV9WYw89aOszqQg5rOCzN+vm00XYts4CXqPGNqSp+LfA/6sO9oDcrL2Bm/AyNjnNOOu41x0MS9HaFgWKopxcSIuWEM2LjbBMeq85EuPbBqnT7hKYVixa0+npRqAAQ3boHvlboIS3M2EyVoi+UmpnFwRighlszaRh4fz+7wJbEZydav11AjJGBB0z155mQF0JaTOZyjX26Vh+Xbst7MqpF08WuL3JSKtr5Ia1sBjnVChViVhmXG3ptRGl3lc4fOBuxc4cPg+KQTWteA+8cvEqn7i1AG401nNQw83Iaiy","/makapicture/user/3915305/images/205462a7ae2c6f78286fc85571828f2f.jpg"];
-	var secret = 'BBpiPuScYpqwrUdZN8sGEgpDU4Cz7kj8XEPddq7VNKMR';
-	var string = arr.join('\n');
-
-	var hmac = crypto.createHmac('sha1', secret);
-	hmac.update(string);
-	var tt = hmac.digest('base64');
-	console.log(tt);
+	var header = {
+		'method': 'PUT',
+		'Content-MD5': 'db5L/shPcEtTJGzpPO3s2w==',
+		'Content-Type': 'image/jpeg',
+		'x-oss-date': 'Tue, 11 Apr 2017 13:08:34 GMT',
+		'x-oss-security-token': '',
+		'x-sdk-client': ''
+	};
+	var path = '';
+	var signature = sign(token, header, path);
+	console.log(signature);
 }
-//crypto.hmac
 
 module.exports = test;

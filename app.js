59bdaf0cf8bb25eab76a0dc3ff74813633d39ddf
zcj <zhengchangjun@eqxiu.com>
2017-05-10 18:08:29 +0800

    bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var api = require('./core/api');
 
-var rabbitpreUrl = 'http://www.rabbitpre.com/template/preview/7af60dfb-43ad-49e6-90c3-d97372993650?mobile=1';
+var rabbitpreUrl = 'http://www.rabbitpre.com/template/preview/43bc7c0d-030c-4c7d-886b-b6bca945c744?mobile=1';
 // var eqxUrl = 'http://h5.eqxiu.com/s//gZHqwAKS';
 var eqxUrl = 'http://h5.eqxiu.com/s//ZlI6UBKr';
 var makaUrl = 'http://viewer.maka.im/pcviewer/T_BJIPAXSV?mode=storeTemplate&TempAdmode=true';

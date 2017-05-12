d9599da578aea4073a99a7960fce54903162d613
zcj <zhengchangjun@eqxiu.com>
2017-05-12 17:32:14 +0800

    'bug'

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var eqxUrl = 'http://h5.eqxiu.com/s//2llzhv6S';
 var makaUrl = 'http://viewer.maka.im/pcviewer/T_BJIPAXSV?mode=storeTemplate&TempAdmode=true';
 // api.makaToRabbit(makaUrl).then(res=>console.log('convert success'));
 // api.rabToEqx(rabbitpreUrl).then(res=>console.log('convert success'));
-// api.eqxToRabbit(eqxUrl)
-// 	.then(res=>console.log('convert success'))
-// 	.catch(err=>console.log(err));
-// api.makaToEqx(makaUrl).then(res=>console.log('convert success'));
-api.eqxToMaka(eqxUrl)
+api.eqxToRabbit('http://h5.eqxiu.com/s//zHacRwam')
 	.then(res=>console.log('convert success'))
 	.catch(err=>console.log(err));
+// api.makaToEqx(makaUrl).then(res=>console.log('convert success'));
+// api.eqxToMaka(eqxUrl)
+// 	.then(res=>console.log('convert success'))
+// 	.catch(err=>console.log(err));
 // api.copyEqx(eqxUrl).then(res => console.log('copy success'));
 // api.copyRabbit(rabbitpreUrl).then(res=>console.log('copy success'));
--- core/api/eqxToRabbit.js
+++ core/api/eqxToRabbit.js
@@ -1,6 +1,6 @@
 var config = require('./../config');
 var Scene = require('./../scene');
-// var db = require('./../db');
+var db = require('./../db');
 var utils = require('./../utils');
 var rabConfig = config.rabbit;
 var Rabbit = require('./../rabbit');
@@ -12,7 +12,7 @@ function eqxToRabbit(url) {
 	var scene = new Scene(url);
 	var json = '{"name":"未命名","tags":"","userid":null,"desc":null,"imgurl":null,"shorturl":null,"imgPath":null,"state":null,"statetime":null,"showviewcount":true,"music_is_auto":1,"timeinterval":0,"comment":0,"logo":null,"logoPath":null,"level":null,"music":null,"musicname":null,"device":"iPhone 5","width":320,"height":504,"in":null,"out":null,"loop":null,"company":null,"link":null,"publish":0,"switch_guide":true,"gather":null,"app_url":null,"templateid":null,"pages":[{"id":null,"row":0,"col":0,"in":null,"out":null,"bgcol":null,"bgimage":null,"bgserver":null,"bgleft":0,"bgtop":0,"cmps":"[]"}]}';
 	return scene.loadData().then(res=>user.login())
-				// .then(res=>utils.checkExist('eqx-rab-' + scene.data.id))
+				.then(res=>utils.checkExist('eqx-rab-' + scene.data.id))
 				.then(res=>{
 					rabbitSevice.setHeaders({
 						Origin: rabConfig.origin, 
@@ -36,8 +36,8 @@ function eqxToRabbit(url) {
 					var json = JSON.parse(res);
 					var rabbit = new Rabbit(json);
 					return scene.toRabbit(rabbit);
-				});
-				// .then(res=>db.put('eqx-rab-' + scene.data.id, url));
+				})
+				.then(res=>db.put('eqx-rab-' + scene.data.id, url));
 }
 
 module.exports = eqxToRabbit;
